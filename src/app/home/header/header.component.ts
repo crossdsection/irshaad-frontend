447c6509518e41d8de4bf6f2cb086e7b7bffe4fe
shivam <shivamsaini@greenusys.com>
2018-06-15 11:43:38 +0530

    Changes in Header , Feeds and tabs

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginsignupComponent } from '../loginsignup/loginsignup.component';
-import { EnactModalComponent } from '../enact-modal/enact-modal.component';
+
 
  @Component({
   selector: 'app-header',
@@ -10,66 +10,14 @@ import { EnactModalComponent } from '../enact-modal/enact-modal.component';
 })
 
 export class HeaderComponent implements OnInit {
- 
-  public govt: boolean = false;
-  public news: boolean = false;
-  public dashboard: boolean = false;
-
-  fcolor = 'transparet';
 
   constructor(private modal: NgbModal) { }
 
-
-  HeaderComponent() {
-    this.bntStyle = 'btn-default';
-  }
-  submit() {
-    this.bntStyle = 'btn-change';
-  }
   onClick() {
     this.modal.open(LoginsignupComponent, { size: 'lg', backdrop: 'static',windowClass:'animated slideInUp'});
   }
-  enactModal() {
-     this.modal.open(EnactModalComponent);
-   }
-
-
+  
   ngOnInit () {  }
 
-  toggle() {
-    this.govt = !this.govt;
-    if (this.govt) {
-      this.dashboard = false;
-      this.news = false;
-    }
-    console.log(this.govt);
-  }
-
-
-  toggle1() {
-    this.news = !this.news;
-    if (this.news) {
-      this.dashboard = false;
-      this.govt = false;
-    }
-    console.log(this.news);
-
-  }
-  toggle2() {
-    this.dashboard = !this.dashboard;
-    if (this.dashboard) {
-      this.govt = false;
-      this.news = false;
-    }
-    console.log(this.dashboard);
-
-  }
-
-
-
-  changeColor(val) {
-    this.fcolor = val;
-    console.log(this.fcolor);
-  }
 
 }
--- src/app/home/tabs/tabs.component.ts
+++ src/app/home/tabs/tabs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EnactModalComponent } from '../enact-modal/enact-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-tabs',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  fcolor = 'transparet';
+  public govt: boolean = false;
+  public news: boolean = false;
+  public dashboard: boolean = false;
 
-  constructor() { }
+  constructor(private modal: NgbModal) { }
 
-  ngOnInit() {
+  enactModal() {
+    this.modal.open(EnactModalComponent);
   }
+  changeColor(val) {
+    this.fcolor = val;
+    console.log(this.fcolor);
+        }
+        ngOnInit () {  }
+        
+          toggle() {
+            this.govt = !this.govt;
+          if (this.govt) {
+            this.dashboard = false;
+            this.news = false;
+          }
+            console.log(this.govt);
+          }
+
+          toggle1() {
+            this.news = !this.news;
+            if (this.news) {
+              this.dashboard = false;
+              this.govt = false;
+            }
+            console.log(this.news);
+
+          }
+          toggle2() {
+            this.dashboard = !this.dashboard;
+            if (this.dashboard) {
+              this.govt = false;
+              this.news = false;
+            }
+            console.log(this.dashboard);
+
+          }
+
 
 }
