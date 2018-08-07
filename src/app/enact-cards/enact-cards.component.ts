4824a7e1802ac04a456cccfbb4f3f53f49c8e1a8
crossdsection <chirkut.kr@gmail.com>
2018-08-07 13:53:20 +0530

    Infinite Scroller Added in Enact Cards Feed

--- src/app/enact-cards/enact-cards.component.ts
+++ src/app/enact-cards/enact-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, OnChanges, Input, ComponentFactoryResolver, ViewChild, ViewContainerRef, HostListener, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { REQUEST_BASE_URL } from '../globals';
 import { LoginPopupComponent } from '../login-popup/login-popup.component';
@@ -15,11 +15,14 @@ export class EnactCardsComponent implements OnChanges {
   @Input() mcph : String = "";
 
   public _loggedIn : Boolean = false;
-  public enactions : Array<Object>;
+  public enactions : Array<Object> = [];
+  public page : number = 1;
+  public currentHeightScrolled : number = 0;
 
   @ViewChild("loginPopupContainer", {read: ViewContainerRef}) loginPopupContainer;
+  @ViewChild("feedContainer", {read: ViewContainerRef}) feedContainer;
 
-  constructor( private http: HttpClient, private componentFactoryResolver: ComponentFactoryResolver ) {
+  constructor( private http: HttpClient, private componentFactoryResolver: ComponentFactoryResolver, private el : ElementRef ) {
     this.http.get(REQUEST_BASE_URL + "user/getinfo").subscribe((response: any) => {
       this._loggedIn = true;
       },
@@ -31,31 +34,50 @@ export class EnactCardsComponent implements OnChanges {
   }
 
   ngOnChanges() {
-    this.getFeeds();
+    this.getFeeds( this.page );
   }
 
-  getFeeds(){
-    var getUrl = REQUEST_BASE_URL + "post/get";
+  @HostListener("window:scroll", ['$event'])
+  onScroll(event: any) {
+    let height = this.feedContainer.element.nativeElement.getBoundingClientRect().height;
+    let scrollTop = -( this.feedContainer.element.nativeElement.getBoundingClientRect().top );
+    if ( ( scrollTop / height ) > 0.75 && this.currentHeightScrolled < height ) {
+      this.page++;
+      this.currentHeightScrolled = height;
+      this.getFeeds( this.page, true ); //true => Change Context
+    }
+  }
 
+  getFeeds( page: number = 1, keepContext = false ){
+    var getUrl = REQUEST_BASE_URL + "post/get";
+    if( !keepContext ){
+      this.currentHeightScrolled = 0;
+      this.page = 1;
+      page = 1;
+    }
     if( this.filter == "bookmark" ) {
       let urlToCall = REQUEST_BASE_URL + "post/getbookmarks";
       let dataToSend = {
         searchKey : "",
-        page : 1,
+        page : page,
         offset : 20
       };
       this.http.post( urlToCall, dataToSend ).subscribe((response: any) => {
         if( response.error == 0 ) {
-          this.enactions = response.data;
-          console.log("Bookmarks");
-          console.log(this.enactions);
+          if( keepContext ){
+            for( var i in response.data ){
+              this.enactions.push( response.data[i] );
+            }
+          } else {
+            this.enactions = response.data;
+          }
         }
       });
       return;
     }
 
     let dataToSend: any = {
-      page: 1
+      page: page
     };
     if( this.mcph != "" ){
       dataToSend['mcph'] = this.mcph;
@@ -68,7 +90,13 @@ export class EnactCardsComponent implements OnChanges {
     }
     this.http.post( getUrl, dataToSend ).subscribe((response: any) => {
       if( response.error == 0 ) {
-        this.enactions = response.data;
+        if( keepContext ){
+          for( var i in response.data ){
+            this.enactions.push( response.data[i] );
+          }
+        } else {
+          this.enactions = response.data;
+        }
       }
     });
   }
