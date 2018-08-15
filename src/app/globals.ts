3ff5524bbed41e95b4010bbd90344e94e75417ca
Kshitij Sharma <010.kshitij@gmail.com>
2018-08-15 11:41:45 +0530

    fixed enact popup

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,4 @@
-export const REQUEST_BASE_URL = "http://localhost/worlvoting/";
-// export const REQUEST_BASE_URL = "https://backend.worldvoting.org/";
+// export const REQUEST_BASE_URL = "http://localhost/worlvoting/";
+export const REQUEST_BASE_URL = "https://backend.worldvoting.org/";
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyAGvUJIs_SRj6bKpbQvNOWHxDjwnSqlvdE';
 export var fileJSON: Array<string> = [];
